79fd4add53d0e28bb0a1be0f5fc336f43c79ecf7
Harsh2621 <147228747+Harsh2621@users.noreply.github.com>
2023-11-06 11:41:27 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,8 @@
+# Accept the radius from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area = 3.14159265359 * (radius ** 2)
+
+# Print the result
+print("Area =", area)
